a8072ead4a796519e3d02fbd69e7b62bd42171a6
rmb97 <rumabe97@gmail.com>
2020-01-29 18:38:18 +0100

    primera subida

---
+++ ProyectoRuben/JavaScript/JavaScript.js
@@ -0,0 +1,8 @@
+function Aparecer () {
+          document.getElementById('zona1').style.visibility="visible";
+          document.getElementById('zona2').style.visibility="hidden";
+		  }
+function Desaparecer () {
+          document.getElementById('zona1').style.visibility="hidden";
+          document.getElementById('zona2').style.visibility="visible";
+      }
