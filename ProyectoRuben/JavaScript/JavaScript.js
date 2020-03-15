87cb4d4315ffd63c0f75d59027be29a18ec038d2
rumabe97 <58530940+rumabe97@users.noreply.github.com>
2020-03-15 22:41:31 +0100

    Add files via upload
    js

--- ProyectoRuben/JavaScript/JavaScript.js
+++ ProyectoRuben/JavaScript/JavaScript.js
@@ -8,5 +8,32 @@ function Desaparecer () {
       }
 	  
 function hora (){
-		document.getElementById('demo').innerHTML = Date();
+		document.getElementById('hora').innerHTML = Date();
+}
+
+function alerta(){
+		alert('Hola ♥♥♥♥');
+}
+
+ function dibujo() {
+
+    var milienzo = document.getElementById("canvas01");
+    var micontexto = milienzo.getContext("2d");
+    var i=0;
+    var x=5;
+    var y=5;
+    var ancho = 390;
+    var alto = 290;
+
+    for (i=0;i<=25;i++) {
+
+        micontexto.strokeStyle="#"+Math.floor(Math.random()*16777215).toString(16);
+        micontexto.strokeRect(x,y,ancho,alto);
+        x=x+5;
+        y=y+5;
+        ancho=ancho-10;
+        alto=alto-10;
+
+    }
+
 }
