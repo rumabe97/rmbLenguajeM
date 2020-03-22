08fa5e354fb5f38e402e2cf169f392a6beaf2fbe
rmb97 <rumabe97@gmail.com>
2020-03-22 21:07:04 +0100

    formulario acabado

---
+++ ProyectoRuben/JavaScript/form.js
@@ -0,0 +1,16 @@
+function Aparecer1 () {
+          document.getElementById('regis').style.visibility="visible";
+		  document.getElementById('regis').style.display="inline-block";
+		  document.getElementById('boton').style.visibility="hidden";
+		  document.getElementById('boton2').style.visibility="hidden";
+}
+
+function Aparecer2 () {
+          document.getElementById('form').style.visibility="visible";
+		  document.getElementById('boton').style.visibility="hidden";
+		  document.getElementById('regis').style.visibility="hidden";
+		  document.getElementById('regis').style.display="none";
+		  document.getElementById('boton2').style.visibility="hidden";
+}
+
+
