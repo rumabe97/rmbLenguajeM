212df0d528de183340381bae3e35a845664015d2
rmb97 <rumabe97@gmail.com>
2020-03-23 15:02:29 +0100

    formulario beta 2

--- ProyectoRuben/JavaScript/form.js
+++ ProyectoRuben/JavaScript/form.js
@@ -1,16 +1,18 @@
 function Aparecer1 () {
           document.getElementById('regis').style.visibility="visible";
-		  document.getElementById('regis').style.display="inline-block";
 		  document.getElementById('boton').style.visibility="hidden";
 		  document.getElementById('boton2').style.visibility="hidden";
+		  document.getElementById('boton').style.display="none";
+		  document.getElementById('boton2').style.display="none";
 }
 
 function Aparecer2 () {
           document.getElementById('form').style.visibility="visible";
 		  document.getElementById('boton').style.visibility="hidden";
-		  document.getElementById('regis').style.visibility="hidden";
 		  document.getElementById('regis').style.display="none";
 		  document.getElementById('boton2').style.visibility="hidden";
+		  document.getElementById('boton').style.display="none";
+		  document.getElementById('boton2').style.display="none";
 }
 
 
