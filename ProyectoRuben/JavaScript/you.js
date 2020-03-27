f1999215bf639bd3b78de2adabfac1eedc685a79
rmb97 <rumabe97@gmail.com>
2020-03-27 19:18:07 +0100

    youtbe beta 2

---
+++ ProyectoRuben/JavaScript/you.js
@@ -0,0 +1,12 @@
+function Aparecer() {
+          document.getElementById('login').style.visibility="visible";
+}
+
+function Mano(){
+		  document.getElementById('gmail').style.cursor = "pointer";
+		  document.getElementById('muchosiconos').style.cursor = "pointer";
+}	
+
+function Desaparecer() {
+          document.getElementById('login').style.visibility="hidden";
+}
